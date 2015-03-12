7c99adeab5ee2e81dce06e54e7f5edc3611825ce
Mike Lorbetske <mike.lorbetske@n3-p.com>
2015-03-11 18:59:46 -0700

    Start of editing nicities

--- JsonValidatorWeb/site.js
+++ JsonValidatorWeb/site.js
@@ -103,7 +103,153 @@
         }
     }
 
+    var tabString = "  ";
+    var tabSize = tabString.length;
+
+
+    function handleEditingKeys(e) {
+        var evt = event || e;
+        var code = evt.keyCode || e.which;
+        var src = evt.srcElement;
+        var selStart = src.selectionStart;
+        var selEnd = src.selectionEnd;
+        var text = src.value;
+
+        switch (code) {
+            case 46: //delete
+                if (!evt.shiftKey) {
+                    return true;
+                }
+
+                var i;
+                for (i = selStart - 1; i >= 0; --i) {
+                    if (text[i] === "\n") {
+                        ++i;
+                        var nextNewLine = text.indexOf("\n", i);
+                        src.value = text.substr(0, i) + (nextNewLine > -1 ? text.substr(nextNewLine + 1) : "");
+                        src.setSelectionRange(i, i);
+                        break;
+                    }
+                }
+
+                if (i === -1) {
+                    var nextNewLine = text.indexOf("\n");
+
+                    if (nextNewLine === -1) {
+                        src.value = "";
+                    } else {
+                        src.value = text.substr(0, nextNewLine);
+                    }
+
+                    src.setSelectionRange(0,0);
+                    break;
+                }
+
+                break;
+            case 36: //home
+                var i;
+                for (i = selStart - 1; i >= 0; --i) {
+                    if (text[i] === "\n") {
+                        ++i;
+                        for (; text[i] === " "; ++i);
+
+                        if (i === selStart) {
+                            return true;
+                        }
+
+                        src.setSelectionRange(i, i);
+                        break;
+                    }
+                }
+
+                if (i === -1) {
+                    ++i;
+                    for (; text[i] === " "; ++i);
+
+                    if (i === selStart) {
+                        return true;
+                    }
+
+                    src.setSelectionRange(i, i);
+                    break;
+                }
+
+                break;
+            case 9: //tab
+                if (!evt.shiftKey) {
+                    src.value = text.substr(0, selStart) + tabString + text.substr(selStart);
+                    src.setSelectionRange(selStart + tabSize, selEnd + tabSize);
+                } else {
+                    var distance = 0;
+                    for (var i = selStart - 1; i >= 0; --i) {
+                        if (text[i] === "\n") {
+                            var initial = i;
+                            ++i;
+                            for (; text[i] === " "; ++i, ++distance);
+                            if (distance > 0) {
+                                if (distance < tabSize) {
+                                    src.value = text.substr(0, initial) + text.substr(i);
+                                    src.setSelectionRange(selStart - distance, selEnd - distance);
+                                } else {
+                                    src.value = text.substr(0, initial + 1) + text.substr(initial + 1 + tabSize);
+                                    src.setSelectionRange(selStart - tabSize, selEnd - tabSize);
+                                }
+                            }
+
+                            break;
+                        }
+                    }
+
+                    if (i === -1) {
+                        ++i;
+                        for (; text[i] === " "; ++i, ++distance);
+                        if (distance > 0) {
+                            if (distance < tabSize) {
+                                src.value = text.substr(i);
+                                src.setSelectionRange(selStart - distance, selEnd - distance);
+                            } else {
+                                src.value = text.substr(tabSize);
+                                src.setSelectionRange(selStart - tabSize, selEnd - tabSize);
+                            }
+                        }
+
+                        break;
+                    }
+                }
+                break;
+            case 13: //return
+                //Determine indent
+                var found = false;
+                for (var i = selStart - 1; i >= 0; --i) {
+                    if (text[i] === "\n") {
+                        var pad = "";
+                        ++i;
+                        for (; text[i] === " "; ++i, pad += " ");
+                        src.value = text.substr(0, selStart) + "\n" + pad + text.substr(selEnd);
+                        src.setSelectionRange(selStart + pad.length + 1, selStart + pad.length + 1);
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    return true;
+                }
+
+                break;
+            default:
+                return true;
+        }
+
+        evt.preventDefault();
+        evt.cancelBubble = true;
+        return false;
+    }
+
+
     elForm.addEventListener("submit", onSubmit, false);
     elInstance.addEventListener("keyup", onInstanceChanged, false);
+    elInstance.addEventListener("keydown", handleEditingKeys, true);
+    elSchema.addEventListener("keydown", handleEditingKeys, true);
 
 })();
